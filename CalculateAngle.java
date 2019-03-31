f43618d1f4322a35dcbcfbe81bca5cf887ab2fc8
51454435 <selfanuj@outlook.com>
2019-03-31 09:57:44 +0530

    first commit

---
+++ CalculateAngle.java
@@ -0,0 +1,27 @@
+package com.general;
+
+public class CalculateAngle {
+
+	public static void main(String[] args) {
+		
+		calculateAngle(12, 30);
+	}
+
+	public static void calculateAngle(double hh, double mm) {
+		double angle = 0;
+		if (hh < 0 || mm < 0 || hh > 12 || mm > 60)
+			System.out.println("Wrong Input:");
+
+		if (hh == 12)
+			hh = 0;
+		if (mm == 60)
+			mm = 0;
+		double hh_angle = (0.5 * (hh * 6 + mm));
+		double mm_angle = (mm * 6);
+		
+		angle = Math.abs(hh_angle-mm_angle);
+		angle = Math.min(360*angle, angle);
+		System.out.println(angle + " "+ "degree");
+	}
+
+}
